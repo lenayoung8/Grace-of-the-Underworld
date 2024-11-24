e1fea68410d0ce58a36e33ead643692cd48f77c4
uwueric <eyang7@vols.utk.edu>
2024-11-24 14:01:06 -0600

    I hope this works :D
    Okay so moving from main to branch does in fact bug things... :( couldn't resolve merge conflicts for inv_ui_slot.gd so I'm copy-pasting the code over

--- grace-of-the-underworld/inventory/inv_ui.gd
+++ grace-of-the-underworld/inventory/inv_ui.gd
@@ -5,6 +5,17 @@ var isOpen = false
 func _ready():
 	close()
 	
+	inv.update.connect(update_slots)
+	update_slots()
+	close()
+	
+# Updates our inventory item slots!
+# Well what does it do specifically...
+# Okay, it updates the UI specifically, since inv.slots IS our ui scene and display
+func update_slots():
+	for i in range(min(inv.slots.size(), slots.size())):
+		slots[i].update(inv.slots[i])
+	
 func _process(delta):
 	if Input.is_action_just_pressed("inventory"):
 		if isOpen:
--- grace-of-the-underworld/inventory/inventory.gd
+++ grace-of-the-underworld/inventory/inventory.gd
@@ -2,4 +2,42 @@ extends Resource
 
 class_name Inventory
 
-@export var items:Array [InventoryItem]
+signal update # I actually dk
+
+# Export the variable into the inspector page
+# the : initializes slots to Array[InvSlot]
+@export var slots:Array [InvSlot]
+
+# Essentially, this function tracks the amount of items
+# in a given slot after it is inserted into the inventory
+func insert(item: InvItem):
+	# Think of itemSlots as an iterator for a set - var itemSlots = ...
+	# essentially goes through the entire inventory and checks if the item is already inside
+	# (i.e. we found the object in inventory & something is already inside),
+	# then we can increment the amount of items in that slot by 1
+	
+	# Below is code to make each slot hold multiple of the same items!
+	# I've just commented it out to work on dropping items from inventory easier!
+	
+	#var itemSlots = slots.filter(func(slot): return slot.item == item)
+	#if !itemSlots.is_empty():
+		#itemSlots[0].amount += 1 # itemSlots[0] as itemSlots returns an array of all slots w/ the given item,
+								 ## so we increment the first one (assumed to be not full... maybe need to add that clause)
+		#
+	## Else, if we couldn't find the item anywhere in inventory,
+	## we use emptySlots to check if there are any empty slots available
+	## Then, if there is, we set the first emptmy slot to the item,
+	## and initialize the amount to 1 (since it's the first appearance of the item!)
+	## Think of this like Superball LMAO
+	#else:
+	
+	# uncomment the above and index all code below by one line up until the update.emit() (which updates the inv after every action)
+	# to add back the multi-stacking!
+	
+	var emptySlots = slots.filter(func(slot): return slot.item == null)
+	if !emptySlots.is_empty():
+		emptySlots[0].item = item
+		emptySlots[0].amount = 1
+			
+	update.emit() # Updates the inventory(?)
+	
